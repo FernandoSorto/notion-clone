332f74be40a355919b7d3846282a136b265155b0
Fernando Sorto <fernando.sorto.10@gmail.com>
2025-01-14 17:04:39 -0600

    Adding collaborative editor

--- components/Document.tsx
+++ components/Document.tsx
@@ -6,6 +6,7 @@ import { Button } from "./ui/button";
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "@/firebase";
 import { useDocumentData } from "react-firebase-hooks/firestore";
+import Editor from "./Editor";
 
 function Document({ id }: { id: string }) {
   const [data, loading, error] = useDocumentData(doc(db, "documents", id));
@@ -30,7 +31,7 @@ function Document({ id }: { id: string }) {
     }
   };
   return (
-    <div>
+    <div className="flex-1 h-full bg-white p-5">
       <div className="flex max-w-6xl mx-auto justify-between pb-5">
         <form className="flex flex-1 space-x-2" onSubmit={updateTitle}>
           {/* Update title */}
@@ -50,7 +51,10 @@ function Document({ id }: { id: string }) {
         {/* Avatars */}
       </div>
 
+      <hr className="pb-10" />
+
       {/* Collaborative editor */}
+      <Editor></Editor>
     </div>
   );
 }
